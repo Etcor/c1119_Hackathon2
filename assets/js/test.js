cbbb5a5bc1024b9c590472fcf9a40b093b527b37
thefullm0nty <montgomery.evan@gmail.com>
2019-12-11 12:27:39 -0800

    Data displaying on dom, links opening in new tabs

--- assets/js/test.js
+++ assets/js/test.js
@@ -8,3 +8,25 @@
 //  });
 //  application.addEventHandlers();
 // }
+var twentyFourHourTime = this.data[index].twentyFourHourTime
+console.log("24h Time from ticketmaster: ", twentyFourHourTime);
+
+var twelveHours = twentyFourHourTime.slice(0, 2);
+console.log("first two digits from: ", twelveHours);
+var minutesAndSeconds = twentyFourHourTime.slice(2);
+console.log("hrs and mins: ", minutesAndSeconds)
+
+if (twelveHours > 12) {
+  var hoursMinusTwelve = twelveHours - 12;
+  var PM = hoursMinusTwelve + " PM"
+  var fixedTime = hoursMinusTwelve + minutesAndSeconds + " PM"
+  // this.$pTagTime = fixedTime;
+  console.log("fixed time: ", fixedTime);
+} else if (twelveHours === '12') {
+  twentyFourHourTime += " PM"
+  // this.$pTagTime = twentyFourHourTime
+  console.log("24 hr time: ", twentyFourHourTime);
+} else {
+  twentyFourHourTime += " AM"
+  // this.$pTagTime = twentyFourHourTime;
+  console.log("24 fixed hours: ", twentyFourHourTime);
