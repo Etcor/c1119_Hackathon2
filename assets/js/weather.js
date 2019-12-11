0092c151091765feda0bc10e654823ee75c34736
chrisward90 <chrsward90@gmail.com>
2019-12-10 19:20:48 -0800

    weather now populates with each search result

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,9 +1,10 @@
 
 class Event_Weather_Current {
-  constructor(lat, lon, parentElem) {
+  constructor(lat, lon, parentElem, index) {
     this.weatherData = {
       lat: lat,
-      lon: lon
+      lon: lon,
+      index: index
     };
     this.parentElem = $(parentElem);
     this.domElements = {
@@ -18,12 +19,12 @@ class Event_Weather_Current {
   }
 
   render() {
-    this.domElements.title = $("<h1>").addClass("weather-title").text("Current Weather");
-    this.domElements.name = $("<h2>").addClass("weather-location-name");
-    this.domElements.tempIconContainer = $("<div>").addClass("temp-and-icon-current");
-    this.domElements.temp = $("<div>").addClass("temp-current");
-    this.domElements.icon = $("<img>").addClass("icon-current");
-    this.domElements.description = $("<h3>").addClass("weather-description");
+    this.domElements.title = $("<h1>").addClass("weather-title weather-title-"+this.weatherData.index).text("Current Weather");
+    this.domElements.name = $("<h2>").addClass("weather-location-name weather-name-" + this.weatherData.index);
+    this.domElements.tempIconContainer = $("<div>").addClass("temp-and-icon-current weather-temp-and-icon-" + this.weatherData.index);
+    this.domElements.temp = $("<div>").addClass("temp-current weather-temp-" + this.weatherData.index);
+    this.domElements.icon = $("<img>").addClass("icon-current weather-icon-" + this.weatherData.index);
+    this.domElements.description = $("<h3>").addClass("weather-description weather-description-" + this.weatherData.index);
 
     this.parentElem.append(this.domElements.title, this.domElements.name, this.domElements.tempIconContainer.append(this.domElements.temp, this.domElements.icon), this.domElements.description);
     // return this.parentElem;
